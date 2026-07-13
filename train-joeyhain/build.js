be182b871c31e8bc492efb91a580b209dbe40303
Joey Hain <59634053+Joey-Hain@users.noreply.github.com>
2026-07-13 17:51:21 +1000

    Add files via upload

--- train-joeyhain/build.js
+++ train-joeyhain/build.js
@@ -69,6 +69,7 @@ const html = `<!doctype html>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Blog — Haintrain</title>
+  <link rel="icon" type="image/x-icon" href="/favicon.jpg">
   <link rel="stylesheet" href="/style.css">
 </head>
 <body>
